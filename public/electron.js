510d42afc6dd6c5663fe3d845e9ff4fb7f521afe
Jakub Horák <jakub.horak@closeit.co>
2020-05-23 21:27:40 +0200

    Adjust window settings

--- public/electron.js
+++ public/electron.js
@@ -8,8 +8,13 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+  });
+  mainWindow.loadURL(
+    isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`
+  )
 
   if (isDev) {
     mainWindow.webContents.openDevTools();
@@ -17,6 +22,23 @@ function createWindow() {
 
   mainWindow.on('closed', () => mainWindow = null);
 }
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
 
 app.on('activate', () => {
   if (mainWindow === null) {
